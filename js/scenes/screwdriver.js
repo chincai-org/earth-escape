901a35bc0a5f0dbc3993adfd64a02093f5f9eb76
ddddddddrrdd3 <108572893+ddddddddrrdd3@users.noreply.github.com>
2025-02-21 21:00:08 +0800

    screwdriver gameplay

--- js/scenes/screwdriver.js
+++ js/scenes/screwdriver.js
@@ -6,8 +6,13 @@ class screwDriver extends Scene {
         this.screwDriverType = -1;
         this.screwType = [[255, 255, 255], [255, 0, 0]];
         this.miss = 0;
+        this.spawnRate = 60;
+        this.gameEnded = false;
     }
     update() {
+        if (this.gameEnded) {
+            return;
+        }
         for (let i = 0; i < this.screws.length; ++i) {
             if (++this.screws[i][3] > 300) {
                 this.screws.splice(i, 1);
@@ -15,18 +20,7 @@ class screwDriver extends Scene {
 
             }
         }
-        if (this.tick % 2 == 0) {
-            let X;
-            let Y;
-            let screw;
-            do {
-                X = this.randint(canvasWidth);
-                Y = this.randint(canvasHeight - 80);
-                screw = this.randint(this.screwType.length);
-            } while (this.screws.some(([x, y]) => Math.hypot(x - X, y - Y) <= (1.1 * this.screwDiameter)));
-            this.screws.push([X, Y + 30, screw, 0]);
-        }
-        this.tick++;
+
         if (mouseIsPressed) {
             for (let i = 0; i < this.screws.length; ++i) {
                 if (Math.hypot(this.screws[i][0] - mouseX, this.screws[i][1] - mouseY) < (this.screwDiameter / 2)) {
@@ -47,6 +41,27 @@ class screwDriver extends Scene {
             default:
                 this.screwDriverType = -1;
         }
+
+        if (this.tick % this.spawnRate == 0) {
+            if (this.spawnRate > 50) {
+                --this.spawnRate;
+            }
+            else {
+                this.spawnRate = 1;
+                if (this.screws.length) return;
+                this.gameEnded = true;
+            }
+            let X;
+            let Y;
+            let screw;
+            do {
+                X = this.randint(canvasWidth - 80);
+                Y = this.randint(canvasHeight - 80);
+                screw = this.randint(this.screwType.length);
+            } while (this.screws.some(([x, y]) => Math.hypot(x - X, y - Y) <= (1.1 * this.screwDiameter)));
+            this.screws.push([X + 40, Y + 60, screw, 0]);
+        }
+        ++this.tick;
     }
     randint(max) {
         return Math.floor(Math.random() * max);
@@ -54,10 +69,13 @@ class screwDriver extends Scene {
 
     draw() {
         background(0)
+        if (this.gameEnded) {
+            this.gameEndDraw();
+            return;
+        }
         textSize(30);
         fill(255, 0, 0);
         textAlign(CENTER, CENTER);
-        const stringMiss = this.miss.toString();
         text("Miss: " + this.miss, canvasWidth / 2, 30);
         noStroke();
         for (let screw of this.screws) {
@@ -67,5 +85,10 @@ class screwDriver extends Scene {
     }
 
 
-    keyPressed() { }
+    gameEndDraw() {
+        fill(255, 255, 255);
+        textAlign(CENTER, CENTER);
+        text("Game over", canvasWidth / 2, canvasHeight / 2);
+        text("Miss: " + this.miss, canvasWidth / 2, canvasHeight / 2 + 60);
+    }
 }
